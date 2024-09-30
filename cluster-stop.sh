6a14ab0a03903fbafeba5e417903310abdf5ebd9
Calin Ardelean <cardelea@cardelea-thinkpadt14sgen2i.tpb.csb>
2024-09-30 13:21:53 +0200

    VM cluster gracefully shutdown

---
+++ cluster-stop.sh
@@ -0,0 +1,13 @@
+#Cluster shutdown 
+
+#!/bin/bash
+
+echo "Mark all the nodes in the cluster as unschedulable"
+for node in $(oc get nodes -o jsonpath='{.items[*].metadata.name}'); do echo ${node} ; oc adm cordon ${node} ; done
+
+echo "Evacuate the pods"
+for node in $(oc get nodes -l node-role.kubernetes.io/worker -o jsonpath='{.items[*].metadata.name}'); do echo ${node} ; oc adm drain ${node} --delete-emptydir-data --ignore-daemonsets=true --timeout=15s --force ; done
+
+echo "Shut down all of the nodes in the cluster"
+for node in $(oc get nodes -o jsonpath='{.items[*].metadata.name}'); do oc debug node/${node} -- chroot /host shutdown -h 1; done 
+

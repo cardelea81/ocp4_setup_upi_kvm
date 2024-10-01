91bf6d19a0cdf69d7d1d57c8117578e6b9964f5f
Calin Ardelean <cardelea@cardelea-thinkpadt14sgen2i.tpb.csb>
2024-10-01 11:17:41 +0200

    cluster start script

---
+++ cluster-start.sh
@@ -0,0 +1,11 @@
+#Start VM's cluster
+
+#!/bin/bash
+
+echo "Basic cluster checks "
+oc get nodes -l node-role.kubernetes.io/master
+oc get nodes -l node-role.kubernetes.io/worker
+oc get clusteroperators
+oc get nodes
+echo "Mark all the nodes in the cluster as schedulable"
+for node in $(oc get nodes -o jsonpath='{.items[*].metadata.name}'); do echo ${node} ; oc adm uncordon ${node} ; done

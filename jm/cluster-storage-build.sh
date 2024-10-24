3127a4449bb464a9fd93b9b79d0e633ff9188da1
Calin Ardelean <cardelea@cardelea-thinkpadt14sgen2i.tpb.csb>
2024-10-24 16:23:56 +0200

    nodes CSR and approve them

--- jm/cluster-storage-build.sh
+++ jm/cluster-storage-build.sh
@@ -26,13 +26,8 @@ sudo $INSTDIR/add_node.sh --cpu 4 --memory 16000 --add-disk 50 --add-disk 100 --
 sudo $INSTDIR/add_node.sh --cpu 4 --memory 16000 --add-disk 50 --add-disk 100 --name storage-3
 
 #nodes CSR and approve them 
-#while true; do
-#  for x in $(/usr/local/bin/oc get csr | grep Pending | awk '{print $1}'); do
-#    /usr/local/bin/oc adm certificate approve $x;
-#  done;
-#  sleep 5;
-#done
-
+for x in $(oc get csr | grep Pending | awk '{print $1}'); do oc adm certificate approve $x; done
+sleep 15
 
 #Label the new nodes
 /usr/local/bin/oc label node storage-1.${CLUSTER_NAME}.${BASE_DOM} cluster.ocs.openshift.io/openshift-storage=''

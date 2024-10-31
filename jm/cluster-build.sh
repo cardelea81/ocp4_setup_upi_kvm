4e3c50e77a46549a4e3161c226d2a81dd72efe61
Calin Ardelean <cardelea@cardelea-thinkpadt14sgen2i.tpb.csb>
2024-10-31 12:03:45 +0100

    Update cluster-build.sh

--- jm/cluster-build.sh
+++ jm/cluster-build.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 #Jenkins Master build
 #Destroy old cluster
+sudo /usr/bin/sed -i.bak '/storage-*/d' /etc/hosts
+#Destroy old cluster
+sudo chown -R hudson:hudson /hudson
 sudo /hudson/ocp4_setup_upi_kvm/ocp4_setup_upi_kvm.sh --cluster-name ocp --cluster-domain lab.example,com --destroy -y
+sudo /hudson/ocp4_setup_upi_kvm/jm/storage-destroy.sh
 
 #Build the new ocp cluster
 sudo /hudson/ocp4_setup_upi_kvm/ocp4_setup_upi_kvm.sh --cluster-name ocp --cluster-domain lab.example.com --ocp-version 4.17.0 --pull-secret /hudson/pull_secret.txt -y
 
 #Change /hudson owner
 sudo chown -R hudson:hudson /hudson
-
-export SDIR="/hudson/ocp4_setup_upi_kvm"
-export SETUP_DIR="/hudson/ocp4_cluster_ocp"
-export DNS_DIR="/etc/NetworkManager/dnsmasq.d"
-export VM_DIR="/var/lib/libvirt/images"
-export KUBECONFIG="/hudson/ocp4_cluster_ocp/install_dir/auth/kubeconfig"
-export CLUSTER_NAME="ocp"
-export BASE_DOM="lab.example.com"
-
 
 #nodes CSR and approve them
 

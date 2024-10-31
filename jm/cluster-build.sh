7f6fe9991521895eb0a606a6700baa7584e33a8f
Calin Ardelean <cardelea@cardelea-thinkpadt14sgen2i.tpb.csb>
2024-10-31 11:47:11 +0100

    Make workernodes storage nodes

--- jm/cluster-build.sh
+++ jm/cluster-build.sh
@@ -8,3 +8,27 @@ sudo /hudson/ocp4_setup_upi_kvm/ocp4_setup_upi_kvm.sh --cluster-name ocp --clust
 
 #Change /hudson owner
 sudo chown -R hudson:hudson /hudson
+
+export SDIR="/hudson/ocp4_setup_upi_kvm"
+export SETUP_DIR="/hudson/ocp4_cluster_ocp"
+export DNS_DIR="/etc/NetworkManager/dnsmasq.d"
+export VM_DIR="/var/lib/libvirt/images"
+export KUBECONFIG="/hudson/ocp4_cluster_ocp/install_dir/auth/kubeconfig"
+export CLUSTER_NAME="ocp"
+export BASE_DOM="lab.example.com"
+
+
+#nodes CSR and approve them
+
+seq 200 | for x in $(/usr/local/bin/oc get csr | grep Pending | awk '{print $1}'); do /usr/local/bin/oc adm certificate approve $x; done
+sleep 20m
+source /hudson/ocp4_cluster_ocp/env
+
+
+
+/usr/local/bin/oc label node storage-1.${CLUSTER_NAME}.${BASE_DOM} cluster.ocs.openshift.io/openshift-storage=''
+/usr/local/bin/oc label node storage-2.${CLUSTER_NAME}.${BASE_DOM} cluster.ocs.openshift.io/openshift-storage=''
+/usr/local/bin/oc label node storage-3.${CLUSTER_NAME}.${BASE_DOM} cluster.ocs.openshift.io/openshift-storage=''
+
+sudo chown -R hudson:hudson /hudson
+
